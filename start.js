aa95ae1b1f5e9cc9d8da792017216beecb539813
Dinusha Niwan  | D006 <121493925+Dinuwa-LK@users.noreply.github.com>
2023-02-04 00:36:19 +0530

    Create start.js

---
+++ start.js
@@ -0,0 +1,14 @@
+
+const testSpeed = require('./main.js');
+
+testSpeed().then((result) => {
+
+  console.log(`Download speed: ${result.downloadSpeed} MB/s`);
+
+  console.log(`Elapsed time: ${result.elapsedTime} seconds`);
+
+}).catch((err) => {
+
+  console.error(err);
+
+});
